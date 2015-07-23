a07281753e6ee7d24b742e83c14e6e4f629f60f9
Gene Taylor <gtaylor@atlassian.com>
2015-07-24 08:16:16 +1000

    move email address to env var

--- fabfile.py
+++ fabfile.py
@@ -8,11 +8,6 @@ from fabric.api import task
 
 @task
 def check_github_dev_blog():
-	github_developer_url = "https://developer.github.com/changes.atom"
-	feed = feedparser.parse( github_developer_url )
-
-	today = datetime.datetime.today().date()
-
 	def contains_keywords( keywords , text ):
 		for keyword in keywords:
 			if keyword in text:
@@ -20,22 +15,25 @@ def check_github_dev_blog():
 		return False
 
 	def sendEmail ( link ):
+		console.log(link)
 		sg = sendgrid.SendGridClient(os.environ.get('SENDGRID_USERNAME'), os.environ.get('SENDGRID_PASSWORD'))
 		message = sendgrid.Mail()
-		message.add_to('gtaylor@atlassian.com')
+		message.add_to(os.environ.get('EMAIL_ADDRESS'))
 		message.set_subject('GitHub developer blog alert')
 		message.set_text(link)
-		message.set_from('GitHub developer blog alerts')
+		message.set_from('changes@GitHubDev')
 		status, msg = sg.send(message)
 
-		
+	github_developer_url = "https://developer.github.com/changes.atom"
+	feed = feedparser.parse( github_developer_url )
+	today = datetime.datetime.today().date()
 	fn = os.path.join(os.path.dirname(__file__), 'keywords.txt')
 
 	with open(fn) as f:
 	    keywords = f.read().splitlines()
 
 	for item in feed["items"]:
-		if dateutil.parser.parse(item["date"]).date() == today:
+		if dateutil.parser.parse(item["date"]).date() < today:
 			if contains_keywords(keywords, item["summary"]):
 				sendEmail(item["link"])
 
